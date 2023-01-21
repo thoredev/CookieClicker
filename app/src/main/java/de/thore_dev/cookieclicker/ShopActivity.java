53a382324e273b391d650766cb7d2ec77933e9ef
thoredev <thorelencer@outlook.de>
2023-01-21 11:35:28 +0100

    Knopffarbe geändert und ShopActivity erstellt

---
+++ app/src/main/java/de/thore_dev/cookieclicker/ShopActivity.java
@@ -0,0 +1,14 @@
+package de.thore_dev.cookieclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
